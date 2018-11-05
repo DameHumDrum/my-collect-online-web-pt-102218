d3906aae3eb3448091d9e3b9c4618d580e3c1e21
Katie Ashlin <akka726@outlook.com>
2018-11-05 16:05:47 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,10 @@
-def _my_collection(array)
-  while
-    yield
-end
+def my_collection(array)
+  i = 0
+  collection= []
+  while i < array.length
+    collection << yield(array[i]) 
+    i = i + 1
+    collection
+  end
+  collection
+end 
